b2177f704e1569d08e1f322214e04ed48787de0d
Nama Kamu <yayohajid@gmail.com>
2025-09-24 14:15:35 +0700

    Update 3

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Aku lembah, baru mau commit!");
+        System.out.println("Aku yayo, commit ke 2 kalinya");
     }
 }
