91f1475cb4feb9443fa31d39241e29a9742622bd
Nama Kamu <yayohajid@gmail.com>
2025-09-24 14:37:33 +0700

    modif 3 yayo

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
         System.out.println("dimodifikasi oleh yayo");
         System.out.println("Dimodifikasi oleh lembah");
         System.out.println("Modif 2 Yayo!");
-        System.out.println("Aku yayo, commit ke 2 kalinya");
+        System.out.println("Aku ini modif ke 3");
         System.out.println("Aku lembah, dan ini commit kedua ku!");
     }
 }
